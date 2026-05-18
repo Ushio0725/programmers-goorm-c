6078179cbba75575604044aa3b35b672b711fcdb
Ushio0725 <minjok6@gmail.com>
2026-05-18 18:49:20 +0900

    2026.05.18_수정
    오늘 문제 총 2개 풀이 완료!
    
    이대로 천천히 느리지만 꾸준하게 잔디 심어보고싶다!

---
+++ goorm-study/goorm-study/02_모임장소_1.c
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h> // abs() 함수와 qsort()를 쓰기 위해 필요해!
+
+    int compare(const void* a, const void* b) {
+        return (*(int*)a - *(int*)b);
+    }
+
+    int main() {
+        int N;
+        scanf("%d", &N);
+
+        int X[100000];
+        int Y[100000];
+
+        for (int i = 0; i < N; i++) {
+            scanf("%d %d", &X[i], &Y[i]);
+        }
+
+        qsort(X, N, sizeof(int), compare);
+        qsort(Y, N, sizeof(int), compare);
+
+        int best_x = X[N / 2];
+        int best_y = Y[N / 2];
+
+        long long total_distance = 0;
+        for (int i = 0; i < N; i++) {
+            total_distance += abs(X[i] - best_x) + abs(Y[i] - best_y);
+        }
+
+        printf("%lld\n", total_distance);
+
+        return 0;
+    }
