0ac290203d822edbe9ce2f5bda91faaac196147b
Ushio0725 <minjok6@gmail.com>
2026-05-18 17:20:08 +0900

    2026.05.18

---
+++ goorm-study/goorm-study/01.정수형_숫자_알기.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char N[100]; // 넉넉하게 배열 100칸을 선언한다.
+
+	scanf("%s", N); // 값을 입력받는다.
+
+	printf("%d\n", strlen(N)); // strlen 함수는 정수을 출력하기 때문에 %d 를 쓴다.
+
+	return 0;
+}
